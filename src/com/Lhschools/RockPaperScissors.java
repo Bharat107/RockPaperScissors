354abdf35a78f01d828365f6e6fa9a439521f53e
Bharat107 <47633551+Bharat107@users.noreply.github.com>
2019-05-23 11:28:53 -0400

    Update RockPaperScissors.java
    Changed variables Fire = rock, water = paper, and ice = scissors

--- src/com/Lhschools/RockPaperScissors.java
+++ src/com/Lhschools/RockPaperScissors.java
@@ -1,4 +1,25 @@
 package com.Lhschools;
+import java.text.DecimalFormat;
 
 public class RockPaperScissors {
-}
+ public enum Item {
+        FIRE, WATER, ICE;
+        public List<Item> inferiorTo;
+        public boolean inferiorTo(Item other) {
+            return inferiorTo.contains(other);
+        }
+        static {
+            ICE.inferiorTo = Arrays.asList(FIRE);
+            FIRE.inferiorTo = Arrays.asList(WATER);
+            WATER.inferiorTo = Arrays.asList(ICE);
+        }
+    }
+    private static DecimalFormat DECIMAL_FORMATTER = new DecimalFormat(".##");
+    public static final Random RANDOM = new Random();
+    private int[] stats = new int[] {0, 0, 0};
+    private int [][] markovChain;
+    private int nbThrows = 0;
+    private Item last = null;
+
+    public static void main(String[] args) {
+    }
